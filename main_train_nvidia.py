361d92cffff1ec660aab9fc5a0c867844ae7a747
f-kuzey-edes-huyal <kuzeyuvercinka+1@gmail.com>
2025-01-06 17:30:01 +0300

    :hammer: Refactor of Nvidia training

--- main_train_nvidia.py
+++ main_train_nvidia.py
@@ -393,21 +393,6 @@ plt.show()
 
 
 
-# In[18]:
-
-
-X_test_reshaped = X_test.reshape(X_test.shape[0], -1)
-print(*loaded_scaler.inverse_transform(X_test_reshaped))
-len(X_test_reshaped[0])
-
-
-
-
-
-
-
-
-# In[ ]:
 
 
 
